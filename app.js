b78fe47b9c91c752e1bd158d678ec1f2ab4ccb22
Maedeh82khosravii <145448883+Maedeh82khosravii@users.noreply.github.com>
2024-01-01 16:10:01 +0330

    update

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 //about this project:
 // developer : maedeh khosravi
 // teacher: MR. khatri
-// start date:1402/10/05
-// end of the project:1402/10/07
+// start date:1402/10/08
+// end of the project:1402/10/11
 // email:khosraviniamaedeh@gmail.com
 
-// یک فانکشن تعیف کردیم .اگر مقداری ک وارد کردیم با اون مقدارمون یکی بود  خروجی این رو نشون بده و ریترنش کنه
+
+// function tarif kardim . k agar meghdar k vard kardim ba ok bod khoroji ro neshon bede 
 function chatrobat(say){
     if (say =="سلام" ||
     say =="hello" ||
