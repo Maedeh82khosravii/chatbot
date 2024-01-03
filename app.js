5cec42756d6817e1f19582afea1a7e7fe91ebde7
Maedeh82khosravii <145448883+Maedeh82khosravii@users.noreply.github.com>
2024-01-03 16:01:32 +0330

    Update app.js
    I defined two functions, one of which takes a value from the input, and the other function performs processing on it, and if the answer of the processing value is the same as the input value, it prints it.

--- app.js
+++ app.js
@@ -6,35 +6,38 @@
 // email:khosraviniamaedeh@gmail.com
 
 
-// function tarif kardim . k agar meghdar k vard kardim ba ok bod khoroji ro neshon bede 
-function chatrobat(say){
-    if (say =="سلام" ||
-    say =="hello" ||
-     say =="hi"
-     ){
-    document.write(" سلام عزیزم  . خوبی؟!" );
-    return;
-}
 
-    else if (say =="مرسی" ||
-    say =="mrc" ||
-    say =="ای میگذره"
-    ){
-    document.write("  خداروشکر . خوبه" );
-    return;
-}
-    else if (say =="چندسالته؟"  
-    ){
-    document.write("نمیدونم خیلی وقته دنیا اومدم" );
-    return;
-}
-
-    else if (say =="" 
-     ){
-        document.write(" جانم؟ " );
-        return;
-}
-
-}
-// prompt tarif kardim k btoinm meghdar bedim
-chatrobat(prompt("ربات خوش امدگویی "))
+// 1- function ra barai daruaft vorodi az karbar tarif maikonim
+function getMessage(){
+    // vorodi ra az karbar grefte va an ra retern kon
+            return input= prompt("درخدمتم خخخ")
+    }
+    // 2- function ra az vorodi grefte va an ra pardazesh konad
+    function processMessage(input){
+    
+    // 3- agar vorodi k az karbar grefte in kalame ra dashte bashad in khoroji ra neshon bede
+        if (input.includes ("سلام")){
+    
+            document.write ("سلام عزیزم ")
+        }
+    
+        else if (input.includes("خوبی")){
+    
+            document.write ("نه تو خوبی فقط")
+        }
+    
+    
+        if (input.includes ("اسمت چیه؟"))
+        {
+            document.write (" نمیدونم ولی خیلیا صدام میزنن ربات نمیدونم چرا  ")
+        }
+    
+        if (input.includes ("چندسالته"))
+        {
+    
+            document.write ("خیلی وقته دنیا اومدم   ")
+        }
+    
+    }
+    getMessage()
+    processMessage(input)
